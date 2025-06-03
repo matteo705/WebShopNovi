0cd04ec0e0136b920fae6f269d2f617580ae4f20
matteo <matteo.madirazza@gmail.com>
2025-06-03 22:54:11 +0200

    Added ApplicationDbContext Added ProductsController Added Product Added frontend for Products Made first migration to DB + update

---
+++ WebShopNovi/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShopNovi.Services;
+
+namespace WebShopNovi.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+    }
+}
