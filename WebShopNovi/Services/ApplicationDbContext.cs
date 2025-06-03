bc0d596846cdc3588b580640bdafb48cabe17823
matteo <matteo.madirazza@gmail.com>
2025-06-03 21:16:12 +0200

    Add project files.

---
+++ WebShopNovi/Services/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using WebShopNovi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebShopNovi.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+        
+        public DbSet<Product> Products {  get; set; }    
+        
+    }
+}
