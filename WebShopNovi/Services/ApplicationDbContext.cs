75e7ecfb8579d8c8a63db935036206602db2781c
matteo <matteo.madirazza@gmail.com>
2025-06-05 23:48:40 +0200

    small bugfixes and code cleaning

--- WebShopNovi/Services/ApplicationDbContext.cs
+++ WebShopNovi/Services/ApplicationDbContext.cs
@@ -15,17 +15,3 @@ namespace WebShopNovi.Services
         
     } 
 }
-
-
-//backup nize
-
-//public class ApplicationDbContext : DbContext
-//{
-//    public ApplicationDbContext(DbContextOptions options) : base(options)
-//    {
-
-//    }
-
-//    public DbSet<Product> Products { get; set; }
-
-//}
